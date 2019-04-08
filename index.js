7c67ac6d17e00776d9c08c585e9eee836b02194a
Adam <iluzjo@gmail.com>
2019-04-08 19:16:03 +0200

    new-js-file

---
+++ index.js
@@ -0,0 +1,41 @@
+//set current time
+const getActualTime = () => {
+    const now = new Date();
+    sec = now.getSeconds();
+    min = now.getMinutes();
+    h = now.getHours() % 12;
+};
+const watch = document.querySelector(".watch");
+const actualTime = () => {
+    watch.textContent = `${h.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+};
+function* incSec(){
+    while(true)
+        yield sec++;
+};
+function* incMin(){
+    while(true)
+        yield min++;
+};
+function* incHour(){
+    while(true)
+        yield h++;
+};
+getActualTime();
+const genSec = incSec();
+const genMin = incMin();
+const genHour = incHour();
+const start = setInterval(() => {
+    genSec.next().value;
+    if (sec===60){
+        sec=0;
+        
+        genMin.next().value;
+        if(min===60){
+            min=0;
+            genHour.next().value;
+            if(h===12)
+                h=0; }} 
+                actualTime(); 
+            }, 1000)
+                     
